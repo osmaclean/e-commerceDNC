424bd9884410d19cd880632462a6487938dc0141
Lucas Maclean <contatolucasmaclean@gmail.com>
2023-06-14 20:11:47 -0300

    fix: adjust first setup

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom";
 import "./index.scss";
+import Header from "../../components/Header/Header";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <section className="login">
+      <Header />
+    </section>
+  );
 }
